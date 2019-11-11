183b8773dd6509c08f2af5e66f6a57618b5a7a0f
mariapereira <mariapereira5a20@gmail.com>
2019-11-11 18:09:22 +0000

    My procedures

--- music_package_grupoI.sql
+++ music_package_grupoI.sql
@@ -366,3 +366,58 @@ BEGIN
 	CALL delete_playlist_PLAYLIST (id);
 	CALL delete_playlist_PLAYLIST_MUSIC (id);
 END;
+
+-- Playlist Music Update
+DROP PROCEDURE IF EXISTS add_music_from_playlist;
+CREATE PROCEDURE add_music_from_playlist (IN playlist_id_in INT, IN music_id_in INT)
+BEGIN
+	INSERT INTO playlist_music (playlist_id, music_id)
+		VALUES (playlist_id_in, music_id_in);
+END;
+
+DROP PROCEDURE IF EXISTS remove_music_from_playlist;
+CREATE PROCEDURE remove_music_from_playlist (IN playlist_id_in INT, IN music_id_in INT)
+BEGIN
+	UPDATE
+		playlist_music SET
+			music_id = music_id_in
+		WHERE
+			playlist_id = playlist_id_in;
+END;
+
+--All Songs and Albums of the selected artist
+DROP PROCEDURE IF EXISTS get_all_songs_from_artist;
+CREATE PROCEDURE get_all_songs_from_artist (IN artist_id_in INT)
+BEGIN
+SELECT music.name, music.duration, music.year, music.explicit, music.spotify_url, music.youtube_url, music.country_id, music.genre_id FROM music INNER JOIN artist_music ON music.music_id = artist_music.music_id WHERE artist_music.artist_id = artist_id_in;
+END; 
+
+DROP PROCEDURE IF EXISTS get_all_albums_from_artist;
+CREATE PROCEDURE get_all_albums_from_artist (IN artist_id_in INT)
+BEGIN
+	SELECT
+		name, `year`, studio_id
+	FROM
+		album
+	WHERE
+		album_id = ( SELECT DISTINCT
+			album_id
+		FROM
+			album_music
+			INNER JOIN artist_music ON album_music.music_id = artist_music.artist_id
+		WHERE
+			artist_music.artist_id = 1);
+END;
+
+-- Top Chart
+DROP PROCEDURE IF EXISTS top_50;
+CREATE PROCEDURE top_50 ()
+BEGIN
+	SELECT
+		*
+	FROM
+		music
+	ORDER BY
+		nr_searchs DESC
+	LIMIT 50;
+END;
