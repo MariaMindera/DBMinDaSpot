28194a4c1bb5d1a1759abf61711f2e2a7655920e
MariaMindera <57108936+MariaMindera@users.noreply.github.com>
2019-11-12 09:44:34 +0000

    Update music_package_grupoI.sql

--- music_package_grupoI.sql
+++ music_package_grupoI.sql
@@ -389,8 +389,14 @@ END;
 DROP PROCEDURE IF EXISTS get_all_songs_from_artist;
 CREATE PROCEDURE get_all_songs_from_artist (IN artist_id_in INT)
 BEGIN
-SELECT music.name, music.duration, music.year, music.explicit, music.spotify_url, music.youtube_url, music.country_id, music.genre_id FROM music INNER JOIN artist_music ON music.music_id = artist_music.music_id WHERE artist_music.artist_id = artist_id_in;
-END; 
+	SELECT
+		music.name, music.duration, music.year, music.explicit, music.spotify_url, music.youtube_url, music.country_id, music.genre_id
+	FROM
+		music
+		INNER JOIN artist_music ON music.music_id = artist_music.music_id
+	WHERE
+		artist_music.artist_id = artist_id_in;
+END;
 
 DROP PROCEDURE IF EXISTS get_all_albums_from_artist;
 CREATE PROCEDURE get_all_albums_from_artist (IN artist_id_in INT)
